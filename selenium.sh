d996cf9bd440a67d6aeb961517006345fbb67c12
H.M.S.Nishantha <srimaln91@gmail.com>
2017-09-17 01:51:14 +0530

    Added shell scripts

---
+++ selenium.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+SERVICE_NAME=selenium
+PATH_TO_JAR=/usr/local/bin/selenium-server-standalone.jar
+CHROME_DRIVER=/usr/local/bin/chromedriver
+PID_PATH_NAME=/tmp/selenium-pid
+START_COMMAND="java -Dwebdriver.chrome.driver=$CHROME_DRIVER -jar $PATH_TO_JAR"
+
+case $1 in
+    start)
+        echo "Starting $SERVICE_NAME ..."
+        if [ ! -f $PID_PATH_NAME ]; then
+            nohup $START_COMMAND /tmp 2>> /dev/null >> /dev/null &
+                        echo $! > $PID_PATH_NAME
+            echo "$SERVICE_NAME started ..."
+        else
+            echo "$SERVICE_NAME is already running ..."
+        fi
+    ;;
+    stop)
+        if [ -f $PID_PATH_NAME ]; then
+            PID=$(cat $PID_PATH_NAME);
+            #echo $PID
+            echo "$SERVICE_NAME stoping ..."
+            pkill $PID;
+            echo "$SERVICE_NAME stopped ..."
+            rm $PID_PATH_NAME
+        else
+            echo "$SERVICE_NAME is not running ..."
+        fi
+    ;;
+    restart)
+        if [ -f $PID_PATH_NAME ]; then
+            PID=$(cat $PID_PATH_NAME);
+            echo "$SERVICE_NAME stopping ...";
+            pkill $PID;
+            echo "$SERVICE_NAME stopped ...";
+            rm $PID_PATH_NAME
+            echo "$SERVICE_NAME starting ..."
+            nohup $START_COMMAND /tmp 2>> /dev/null >> /dev/null &
+                        echo $! > $PID_PATH_NAME
+            echo "$SERVICE_NAME started ..."
+        else
+            echo "$SERVICE_NAME is not running ..."
+        fi
+    ;;
+    status)
+        if [ -f $PID_PATH_NAME ]; then
+            echo "Service is running."
+        else
+            echo "Service is not running."
+        fi
+    ;;
+esac
---
+++ selenium_install.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#Variables
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+#Need to run this script as a root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root (with sudo command)"
+  exit
+fi
+
+#Update packages and install required ones
+apt-get -y update
+apt-get install -y unzip
+
+#Install Java runtime
+if [ ! -n `which java` ]; 
+    then apt-get install -y default-jre
+fi
+
+#Download and install Chrome web driver
+VERSION="$(uname -m)"
+
+if [ "${VERSION}" == "x86_64:" ]; then
+	CHR_DRIVER="https://chromedriver.storage.googleapis.com/2.32/chromedriver_linux32.zip"
+else
+	CHR_DRIVER="https://chromedriver.storage.googleapis.com/2.32/chromedriver_linux64.zip"
+fi
+
+TEMP_PATH="/tmp/chromedriver"
+mkdir $TEMP_PATH && pushd $TEMP_PATH
+wget $CHR_DRIVER
+unzip ${TEMP_PATH}/chromedriver_linux64.zip
+sudo mv -f ./chromedriver /usr/local/bin/chromedriver
+sudo chown root:root /usr/local/bin/chromedriver
+sudo chmod 0755 /usr/local/bin/chromedriver
+
+printf "${GREEN}Installed Chrome web driver.\n${NC}"
+
+#download and install selenium server
+SELENIUM_VERSION="3.5.3"
+SELENIUM_JAR_FILE=selenium-server-standalone-$SELENIUM_VERSION.jar
+wget http://selenium-release.storage.googleapis.com/$(echo $SELENIUM_VERSION | cut -d'.' -f-2)/$SELENIUM_JAR_FILE
+mv -f $SELENIUM_JAR_FILE /usr/local/bin
+chown root:root /usr/local/bin/$SELENIUM_JAR_FILE
+chmod 755 /usr/local/bin/$SELENIUM_JAR_FILE
+
+printf "${GREEN}Installed Selenium server.\n${NC}"
+
+# Get back to current directory
+popd
+
+# Setup init script
+cp ./selenium.sh /usr/local/bin/selenium
+chmod +x /usr/local/bin/selenium
+
+# Setup systemd service
+cp selenium /etc/systemd/system/selenium.service
+chown root:root /etc/systemd/system/selenium.service
+chmod 755 /etc/systemd/system/selenium.service
+
+printf "${GREEN}Installation is successful.\n${NC}"
